a5fee566c55fb9413ef07b9e7581b7c9c8864e85
wdg <147423661@qq.com>
2017-02-26 20:51:26 +0800

    Signed-off-by: wdg <147423661@qq.com>

---
+++ (G)分布式计算PYTHON例子代码(可用在回测)/taskmanager.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#1.本文件及调用的Python文件为范例代码
+#2.另期货PYTHON程序化交易框架遵循 开源协议GPL v3
+#简单的说：对基于GPL v3协议的源代码，若个人或机构仅仅是自己使用，则可以闭源。
+#若基于该开源代码，开发出程序或衍生产品用于商业行为则也必须开源。
+
+#Quciklib Python框架和工具遵循GPL v3协议包括：
+#（1）Quicklib CTP   期货行情库
+#（2）Quicklib CTP   期货交易库
+#（3）Quicklib CTP2  A股行情库
+#（4）Quicklib MOM模式  博易资管交易库
+#（用于接入资管投顾系统，MOM模式可实现私募进行投顾的选拔考核，并通过自己的风控系统接入实盘）
+
+#Quciklib Python框架和工具暂不遵循开源协议包括：
+#（5）Quicklib 监控器库（预警、监控、交易信号数据复制、跟单）（可免费试用）
+
+#对Quciklib开源库做出贡献的，并得到原始作者肯定的，将公布在http://www.quciklib.cn网站上，
+#并添加在《开源说明和感谢.txt》，并将该文件不断更新放入每一个新版本的Quicklib库里。
+
+#原始作者：QQ 147423661 林
+#官方网站：http://www.quicklib.cn
+#官方QQ群：5172183(1群)、25884087(2群)
+import random, time
+#import queue
+import Queue
+from multiprocessing import freeze_support
+from multiprocessing.managers import BaseManager
+
+# 发送任务的队列:
+task_queue = Queue.Queue()
+# 接收结果的队列:
+result_queue = Queue.Queue()
+
+# 从BaseManager继承的QueueManager:
+class QueueManager(BaseManager):
+    pass
+
+def return_task_queue():
+    global task_queue
+    return task_queue
+
+def return_result_queue():
+    global result_queue
+    return result_queue
+
+def test():
+    # 把两个Queue都注册到网络上, callable参数关联了Queue对象:
+    # QueueManager.register('get_task_queue', callable=lambda: task_queue)
+    # QueueManager.register('get_result_queue', callable=lambda: result_queue)
+    QueueManager.register('get_task_queue', callable=return_task_queue)
+    QueueManager.register('get_result_queue', callable=return_result_queue)
+
+    # 绑定端口5000, 设置验证码'abc':
+    manager = QueueManager(address=('192.168.0.201', 5000), authkey=b'abc')
+    # 启动Queue:
+    manager.start()
+    # 获得通过网络访问的Queue对象:
+    task = manager.get_task_queue()
+    result = manager.get_result_queue()
+    # 放几个任务进去:
+    for i in range(10):
+        n = random.randint(0, 10000)
+        print('Put task %d...' % n)
+        task.put(n)
+    # 从result队列读取结果:
+    print('Try get results...')
+    for i in range(10):
+        r = result.get(timeout=10)
+        print('Result: %s' % r)
+    # 关闭:
+    manager.shutdown()
+    print('master exit.')
+
+if __name__ == '__main__':
+    freeze_support()
+    test()
